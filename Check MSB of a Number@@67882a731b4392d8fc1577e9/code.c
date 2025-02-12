a41727ce01f6de419e9a1fb09cb23fffb8333ee6
Samarth Sharma <samarthsharma1411@gmail.com>
2025-02-12 17:14:23 +0530

    Solution to Check MSB of a Number

--- Check MSB of a Number@@67882a731b4392d8fc1577e9/code.c
+++ Check MSB of a Number@@67882a731b4392d8fc1577e9/code.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
-#define BITS sizeof(int) * 8 // Total bits required to represent integer
+#define BITS sizeof(int) * 8
 
 int main()
 {
     int num, msb;
 
-    /* Input number from user */
     printf("Enter any number: ");
     scanf("%d", &num);
 
-    /* Move first bit of 1 to highest order */
     msb = 1 << (BITS - 1);
 
-    /* Perform bitwise AND with msb and num */
     if(num & msb)
         printf("Set");
     else
